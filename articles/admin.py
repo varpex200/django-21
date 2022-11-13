70ba9adcb0bb6a87f52318712db7b4d75bba47f9
Saeid Dadgar <saeidrd90@gmail.com>
2022-11-13 18:20:39 +0330

    Latest Changes

--- articles/admin.py
+++ articles/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'title']
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
--- articles/models.py
+++ articles/models.py
@@ -14,6 +14,18 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
+class Comment(models.Model):
+    text = models.TextField(verbose_name='متن کامنت')
+    related_user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر مربوطه')
+    related_article = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name='مقاله مربوطه')
+
+    class Meta:
+        verbose_name = 'کامنت'
+        verbose_name_plural = 'کامنت ها'
+    
+    def __str__(self):
+        return f'{self.related_user}: {self.text}'
+
 class Car(models.Model):
     title = models.CharField(max_length=64, verbose_name='عنوان')
     description = models.TextField(verbose_name='توضیحات')
